b577f1b2450897541e231a8ca323cc37df4c2f41
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2024-04-17 00:05:34 +0300

    Delete test.cpp

--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include "Amino/Client.h"
-#include <typeinfo>
-int main() {
-    Client client("1955B43A1D7149596FA7B8636F6C0A3785D81AA219ED04E26E9CEF2F303892DB25C7150845F1649A6E");
-    //std::cout << client.login();
-
-    return 0;
-}
