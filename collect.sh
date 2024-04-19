90f08f13ed87e32596ba578b74ddaa83ff252b51
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2024-04-19 13:45:05 +0300

    Add files via upload

---
+++ collect.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir build
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create the build directory."
+    exit 1
+fi
+cd build
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to change directory to build."
+    exit 1
+fi
+cmake ..
+if [ $? -ne 0 ]; then
+    echo "Error: cmake exited with an error."
+    exit 1
+fi
+make
+if [ $? -ne 0 ]; then
+    echo "Error: make exited with an error."
+    exit 1
+fi
+
+echo "The library has been built. The file is located at build/lib/libAmino.a."
